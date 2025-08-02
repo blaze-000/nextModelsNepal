4481eb9032e2853ba2c580b96ae7d2a0388715f3
Nabin <nabinpoudel788@gmail.com>
2025-08-02 21:27:47 +0545

    feat(Models): Define Mmodels Schema and model

---
+++ backend/src/models/contact.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    subject: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    message: { type: String, required: true },
+    status: {
+        type: String,
+        enum: ['new', 'read', 'replied', 'archived'],
+        default: 'new'
+    },
+    replied: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+// Update the updatedAt field on save
+contactSchema.pre('save', function (next) {
+    this.updatedAt = new Date();
+    next();
+});
+
+export const ContactModel = mongoose.model("ContactModel", contactSchema); 
