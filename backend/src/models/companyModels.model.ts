e1daf3831c68f3481ebcfa5913901b1c1939f782
Nabin <nabinpoudel788@gmail.com>
2025-08-04 20:58:17 +0545

    feat(Models-Form): Define Models intro schema and rename image key variable

---
+++ backend/src/models/companyModels.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const COMMODELSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    intro: {type: String, required: true},
+    address: { type: String, required: true },
+    coverImage: {type: String, required: true},
+    images: { type: [String], default: [] },
+    gender: { type: String, required: true }
+});
+
+export const COMMODEL = mongoose.model("COMMODEL", COMMODELSchema);
