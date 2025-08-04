628e794e2ac32ea4be5541f0bbdf3f533c523c66
Nabin <nabinpoudel788@gmail.com>
2025-08-04 21:01:58 +0545

    feat(Hire-Model): remove unnecessary schema.

--- backend/src/models/hireModel.model.ts
+++ backend/src/models/hireModel.model.ts
@@ -1,24 +1,13 @@
 import mongoose from "mongoose";
 
 const hireSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    subject: { type: String, required: true },
+    name: {type: String},
+    model: { type: mongoose.Schema.Types.ObjectId, ref: "COMMODEL", required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
     message: { type: String, required: true },
     date: { type: Date, required: true },
-    model: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "ModelsModel",
-        required: true
-    },
-    replyMessage: { type: String },
-    status: { 
-        type: String, 
-        enum: ['Pending', 'Replied', 'Approved', 'Rejected', 'Under Review'],
-        default: 'Pending'
-    },
-    repliedAt: { type: Date },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
