d10992d20ea89316ffa649d3196f17b9311f97c9
Nabin <nabinpoudel788@gmail.com>
2025-08-06 16:48:48 +0545

    feat(validation): refactor

--- backend/src/validations/next.event.validation.ts
+++ backend/src/validations/next.event.validation.ts
@@ -1,33 +1,29 @@
 import { z } from "zod";
 
+// Card Item Schema
+const cardItemSchema = z.object({
+  index: z.string().optional(),
+  criteriaTitle: z.string().min(1, "Criteria title is required"),
+  criteria: z.string().min(1, "Criteria is required"),
+  criteriaIcon:  z.string().min(1, "Title image is required"),
+});
+
+// Card Schema
+const cardSchema = z.object({
+  index: z.string().optional(),
+  cardTitle: z.string().min(1, "Card title is required"),
+  item: z.array(cardItemSchema).default([])
+});
+
+// Main Event Schema
 export const nextEventSchema = z.object({
-    title: z.string().optional(),
-    heading: z.string().optional(),
-    description: z.string().optional(),
-    images: z.array(z.string()).optional(),
-
-    subtitle: z
-        .array(
-            z.object({
-                index: z.string().optional(),
-                name: z.string("Subtitle name is required."),
-                icon: z.array(z.string()).optional(),
-
-                items: z
-                    .array(
-                        z.object({
-                            index: z.string().optional(),
-                            tag: z.string().optional(),
-                            tagContent: z.string().optional(),
-                            tagIcon: z.array(z.string()).optional(),
-                        })
-                    )
-                    .optional()
-                    .default([]),
-            })
-        )
-        .optional()
-        .default([]),
-
-    notice: z.array(z.string()).optional().default([]),
+  tag: z.string().min(1, "Tag is required"),
+  title: z.string().min(1, "Title is required"),
+  titleImage: z.string().min(1, "Title image is required"),
+  image: z.string().min(1, "Image is required"),
+  description: z.string().min(1, "Description is required"),
+  noticeName: z.string().min(1, "Notice name is required"),
+  notice: z.array(z.string().min(1, "Notice item cannot be empty"))
+    .min(1, "At least one notice is required"),
+  card: z.array(cardSchema).default([])
 });
