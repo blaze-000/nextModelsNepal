7103d2b063ab052244ede34232a911df9f1b4ee2
PERIS <periskc@gmail.com>
2025-08-12 23:34:44 +0545

    Feedback(testimonial) model changed

--- backend/src/models/feedback.model.ts
+++ backend/src/models/feedback.model.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const feedbackSchema = new mongoose.Schema({
-    index: { type: String, required: true },
+    index: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     message: { type: String, required: true },
-    image: { type: String },
-})
+    image: { type: String, unique: true },
+}, { timestamps: true });
 
 export const FeedBackModel = mongoose.model("FeedBackModel", feedbackSchema);
