feadcb82b6c0f4d7d83462b58815f8d7e9744246
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2025-08-17 11:33:57 +0545

    added dropdown and eventbox

--- frontend/src/app/(public)/events/past-events/page.tsx
+++ frontend/src/app/(public)/events/past-events/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Winners } from "@/components/events/winners";
-import ImageBox from "@/components/molecules/image-box";
+import EventBox from "@/components/molecules/event-box";
 import Breadcrumb from "@/components/molecules/breadcumb";
 import React from "react";
 import { motion } from "framer-motion";
@@ -14,32 +14,31 @@ export default function PastEvents() {
       title: "Mister. Nepal",
       description:
         "Next Models Nepal leads Nepal’s fashion and entertainment scene—discovering talent, creating iconic events, and shaping industry trends.",
-      link: "#",
+      slug: "#",
     },
     {
       id: 2,
       image: "/news_1.jpg",
-      title: "Next Models Nepal Hosts Successful Fashion Week Event",
+      title: "Mister. Nepal",
       description:
         "A spectacular showcase of emerging designers and models, setting new standards for the Nepalese fashion industry.",
-      link: "#",
+      slug: "#",
     },
     {
       id: 3,
       image: "/news_1.jpg",
-      title:
-        "Bivash Bista and Neha Budha Crowned Winners of Model Hunt Nepal Season 9",
+      title: "Mister. Nepal",
       description:
         "Our recent fashion show made headlines, showcasing Nepal's emerging talent pool in the modeling industry.",
-      link: "#",
+      slug: "#",
     },
     {
       id: 4,
       image: "/news_1.jpg",
-      title: "Next Models Nepal Hosts Successful Fashion Week Event",
+      title: "Mister. Nepal",
       description:
         "A spectacular showcase of emerging designers and models, setting new standards for the Nepalese fashion industry.",
-      link: "#",
+      slug: "#",
     },
   ];
 
@@ -61,12 +60,13 @@ export default function PastEvents() {
                 transition={{ duration: 0.6, delay: 0.2 }}
                 viewport={{ once: true, amount: 0.1 }}
               >
-                <ImageBox
+                <EventBox
+                  slug={item.slug}
+                  status={"ended"}
                   image={item.image}
                   title={item.title}
                   desc={item.description}
-                  link={item.link}
-                  buttonText={`About ${item.title}`}
+                   buttonText={`About ${item.title}`}
                 />
               </motion.div>
             ))}
