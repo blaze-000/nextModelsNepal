4329cfeb2d3e4bd71586d6592e599696325d4447
PERIS <periskc@gmail.com>
2025-08-17 15:42:33 +0545

    next config fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,19 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-   
+
     remotePatterns: [
       {
         protocol: "http",
         hostname: "192.168.68.105",
         port: "8000",
-     
+
       },
       {
         protocol: "https",
         hostname: "placehold.co",
-        
-       
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
       }
     ],
   },
