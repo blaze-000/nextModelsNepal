5b727d5d499a496354f7c7e814c82b68d35b6832
Nabin <nabinpoudel788@gmail.com>
2025-08-20 17:05:44 +0545

    feat(payment): add better date format

--- backend/src/utils/format.ts
+++ backend/src/utils/format.ts
@@ -2,10 +2,9 @@ import dayjs from 'dayjs';
 
 // MM/DD/YYYY
 export function formatFonepayDate(d: Date = new Date()) {
-  return dayjs(d).format('MM/DD/YYYY');
+  return dayjs(d).format("MM/DD/YYYY");
 }
 
-// Ensure amount format with up to 2 decimals, dot decimal mark, no thousands
 export function formatAmount(v: number) {
   return v.toFixed(2);
 }
