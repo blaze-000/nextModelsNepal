2b2b4a11269c3b37801e891b4571f9e2dba6c139
Sachit Yadav <sachityadav987@gmail.com>
2025-11-07 15:44:42 +0545

    Update Next.js image remote patterns
    Changed remotePatterns to use nextmodelsnepal.com for HTTPS and updated
    localhost to use port 8000 for HTTP.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,26 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "192.168.68.103",
-        port: "8000",
-
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-      }
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "nextmodelsnepal.com",
+            },
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "8000",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
